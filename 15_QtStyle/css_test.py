44a7c5ce0464d2a6ce75c2558123cafb0254c0cb
Unknown <miklem3d@gmail.com>
2018-10-07 23:48:22 +0300

    CSS added
    + background color change

--- 15_QtStyle/css_test.py
+++ 15_QtStyle/css_test.py
@@ -1,12 +1,17 @@
 from PySide.QtCore import *
 from PySide.QtGui import *
 from widgets import css_test_uis as ui
+import os
+
+style = os.path.join(os.path.dirname(__file__), 'style.css')
 
 
 class StyleWidgetClass(QMainWindow, ui.Ui_testUi):
     def __init__(self):
         super(StyleWidgetClass, self).__init__()
         self.setupUi(self)
+        # check if the file exists
+        self.setStyleSheet(open(style).read())
         
         
 if __name__ == '__main__':
