1f97860faf70c3484528ca7f4171d9e166404e33
Mikhail M <miklem3d@gmail.com>
2018-10-09 11:50:28 +0300

    CSS
    + added css

--- 15_QtStyle/css_test.py
+++ 15_QtStyle/css_test.py
@@ -1,13 +1,20 @@
 from PySide.QtCore import *
 from PySide.QtGui import *
 from widgets import css_test_uis as ui
+import os
+
+style = os.path.join(os.path.dirname(__file__), 'style.css')
 
 
 class StyleWidgetClass(QMainWindow, ui.Ui_testUi):
     def __init__(self):
         super(StyleWidgetClass, self).__init__()
         self.setupUi(self)
-        
+        self.pushButton.clicked.connect(self.assignCSS)
+
+    def assignCSS(self):
+        self.setStyleSheet(open(style).read())
+
         
 if __name__ == '__main__':
     app = QApplication([])
