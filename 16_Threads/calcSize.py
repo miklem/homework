8cd4d1d40b2615ec260032cd2a64897d08cc58cd
Mikhail M <mmiroshnichenko@croc.ru>
2018-11-29 19:31:44 +0300

    Thread run
    +start calculation in new thread

--- 16_Threads/calcSize.py
+++ 16_Threads/calcSize.py
@@ -3,6 +3,7 @@ from PySide.QtGui import *
 import os
 
 
+
 class folderSizeCompute(QWidget):
     def __init__(self):
         super(folderSizeCompute, self).__init__()
@@ -18,15 +19,35 @@ class folderSizeCompute(QWidget):
         self.resize(300, 100)
 
     def calc(self):
-        size = 0
-        for path, dirs,files in os.walk(self.le.text()):
-            for f in files:
-                b = os.path.getsize(os.path.join(path, f))
-                size += int(b/1024.0)
-        self.setInfo(size)
+        path = self.le.text()
+        self.obj = Worker(path)
+        self.t = QThread()
+        self.obj.moveToThread(self.t)
+        self.t.started.connect(self.obj.start)
+        self.obj.finishedSignal.connect(self.t.quit)
+        self.obj.finishedSignal.connect(self.setInfo)
+        self.t.start()
+
+
 
     def setInfo(self, size):
         self.lb.setText('%s bytes' % size)
+
+
+class Worker(QObject):
+    finishedSignal = Signal(int)
+    def __init__(self, path):
+        super(Worker, self).__init__()
+        self.path = path
+
+    def start(self):
+        size = 0
+        for path, dirs,files in os.walk(self.path):
+            for f in files:
+                b = os.path.getsize(os.path.join(path, f))
+                size += int(b/1024.0)
+        self.finishedSignal.emit(size)
+
 
 
 if __name__ == '__main__':
