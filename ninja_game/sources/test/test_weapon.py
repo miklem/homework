d925f07119a02917756a879d1703cc3278bb9974
Mikhail M <mmiroshnichenko@croc.ru>
2018-12-11 19:48:00 +0300

    Game functions tests

--- ninja_game/sources/test/test_weapon.py
+++ ninja_game/sources/test/test_weapon.py
@@ -10,5 +10,68 @@ class TestWeaponDispenser(unittest.TestCase):
         -------
 
         """
+        wd = WeaponDispenser()
 
-        w = WeaponDispenser()
+    def test_num_weapons(self):
+        """
+        WeaponDispenser should start with the correct number of weapons
+
+        Returns
+        -------
+
+        """
+        wd = WeaponDispenser()
+        self.assertTrue(len(wd.weapons) == 3,
+                    "There are not 3 weapons in dispenser")
+
+    def test_dispense_weapon(self):
+        """
+        WeaponDispenser dispenses next weapon
+
+        Returns
+        -------
+
+        """
+
+        wd = WeaponDispenser()
+        top_of_stack = wd.weapons[-1]
+        if wd.dispense() != top_of_stack:
+            self.fail("We got a wrong weapon!")
+
+    def test_empty_dispenser(self):
+        """
+        empty dispenser dispenses none
+
+        Returns
+        -------
+
+        """
+        wd = WeaponDispenser()
+        for i in range(len(wd.weapons)):
+            wd.dispense()
+        self.assertFalse(wd.dispense(),
+                         "Empty dispenser!")
+
+    # @unittest.skip("TODO: Need designer input about future stuff")
+    def test_future_stuff(self):
+        """
+        Future stuff
+
+        Returns
+        -------
+
+        """
+        self.fail("Failed for the purpose")
+
+    @unittest.expectedFailure
+    def test_easter_egg(self):
+        """
+        Easter egg...
+
+        Returns
+        -------
+
+        """
+
+        wd = WeaponDispenser()
+        wd.easter_egg()
--- ninja_game/sources/weapon.py
+++ ninja_game/sources/weapon.py
@@ -1,2 +1,12 @@
 class WeaponDispenser:
-    pass
+    def __init__(self):
+        self.weapons = ["katana", "suriken", "poison"]
+
+    def dispense(self):
+        if self.weapons:
+            return self.weapons.pop()
+        return None
+
+    def easter_egg(self):
+        # return "Eggs!"
+        raise NotImplemented
